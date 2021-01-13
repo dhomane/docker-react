1d79000db81e4678c9cc10ede40f50bd75cee2ba
Dhomane <dhomane@147dda3b9f43.ant.amazon.com>
2021-01-13 07:09:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save this Omkar Dhomane, Offer Letter from Google Technical Solutions Engineer and to reload.
         </p>
         <a
           className="App-link"
