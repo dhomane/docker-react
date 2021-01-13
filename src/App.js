19ed255bbdac64d2f37fce596ba3404bce253acf
Dhomane <dhomane@147dda3b9f43.ant.amazon.com>
2021-01-13 08:42:21 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save this Omkar Dhomane, Offer Letter from Google Technical Solutions Engineer and to reload.
+          Edit <code>src/App.js</code> and save this Omkar Dhomane, Google this and then reload.
         </p>
         <a
           className="App-link"
